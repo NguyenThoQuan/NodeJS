0e2b4d7b59784752160b476171e0232e9b26341d
NguyenThoQuan <thoquan150903@gmail.com>
2024-07-11 16:57:29 +0700

    49

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,27 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+import { MongoClient, ServerApiVersion } from "mongodb";
+import { env } from "./environment";
+
+let nodeJSDatabaseInstance = null;
+
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+export const CONNECT_DB = async () => {
+  await mongoClientInstance.connect();
+  nodeJSDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME);
+};
+
+export const GET_DB = () => {
+  if (!nodeJSDatabaseInstance)
+    throw new Error("Must connect to Database first !");
+  return nodeJSDatabaseInstance;
+};
+
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close();
+};
